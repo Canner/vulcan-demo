eee686f56963477507936ff6295661b61a63df0d
Ivan Tsai <9553914+oscar60310@users.noreply.github.com>
2022-09-19 23:16:44 +0800

    add customers table

--- data/createdb.js
+++ data/createdb.js
@@ -19,7 +19,40 @@ async function runQuery(query) {
 }
 
 (async () => {
-  console.log('inserting BankChurners.csv ...');
-  await runQuery(`create table bank_churners as select * from "BankChurners.csv"`);
+  console.log('inserting churners.csv ...');
+  await runQuery(`
+  create table churners as 
+  select 
+    cast(CLIENTNUM as varchar) as id,
+    Customer_Age as age,
+    Gender as gender,
+    Education_Level as education_level,
+    Marital_Status as marital_status,
+    Income_Category as income_category,
+    Card_Category as card_category,
+    Months_on_book as months_on_book,
+    Total_Relationship_Count as total_relationship_count,
+    Months_Inactive_12_mon as months_inactive_12_mon,
+    Contacts_Count_12_mon	as contacts_count_12_mon,
+    Credit_Limit as	credit_limit,
+    case
+      when Attrition_Flag = 'Existing Customer' 
+        then false
+        else true 
+    end as attrited
+  from "churners.csv"
+  `);
+  console.log('done.');
+
+  console.log('inserting customers.csv ...');
+  await runQuery(`
+  create table customers as 
+  select 
+    cast(id as varchar) as id,
+    first_name,
+    last_name,
+    email
+  from "customers.csv"
+  `);
   console.log('done.');
 })();
